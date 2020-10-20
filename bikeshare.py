959de3dea5940c1eaa8ef1cb43f14b6b33ff35c7
Sandra Kargus <sandra.Kargus@me.com>
2020-10-20 10:49:29 +0200

    Refactoring in def get_filters

--- bikeshare.py
+++ bikeshare.py
@@ -15,15 +15,97 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    print('Hello! Nice to see you. Let\'s explore some US bikeshare data!')
 
+    #set filter_criteria standard to all
+    month = 'all'
+    day = 'all'
 
-    # TO DO: get user input for month (all, january, february, ... , june)
+    # To get user input for city (chicago, new york city, washington).
+    while True:
+        try:
+            city = str(input('\nWould you like to see data for Chicago, New York or Washington?\n').lower())
+            if city == 'washington' or city == 'chicago' or city == 'new york':
+                if city == 'new york':
+                    city = 'new york city'
+                break
+            else:
+                print('\nYou\'re input is not valid. Please close the program if you want to stop.\n')
+        except:
+            print('\ninvalid value\n')
 
+    print('\n{} is your choosen filter.\n'.format(city.title()))
 
-    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
+    # To get user input for filter criteria month, day or no filter.
+    while True:
+        try:
+            filter_criteria = str(input('Would you like to filter the data by month (type: month), day (type: day), both (type: both) or not at all (type: all)?\n').lower())
+            if filter_criteria == 'month' or filter_criteria == 'day' or filter_criteria == 'both' or filter_criteria == 'all':
+                break
+            else:
+                print('\n You\'re input is not valid. Please close the program if you want to stop.\n')
+        except:
+            print('\ninvalid value\n')
 
+    print('\n{} is your choosen filter.\n'.format(filter_criteria).title())
+
+    # to get user input for month + day of the week
+    if filter_criteria == 'both':
+        # To get user input for month (all, january, february, ... , june)
+        while True:
+            try:
+                month = str(input('Which month do you like to filter. Please type: January, February, March, April, Mai, June or all?\n').lower())
+                if month == 'january' or month == 'february' or month == 'march' or month == 'april' or month == 'may' or month == 'june' or month == 'all':
+                    break
+                else:
+                    print('\nYou\'re input is not valid. Please close the program if you want to stop.\n')
+            except:
+                print('\nThat\'s not a valid number!\n')
+
+        print('\n{} is your choosen filter.\n'.format(month.title()))
+
+        # To get user input for day of week (all, monday, tuesday, ... sunday)
+        while True:
+            try:
+                day = str(input('\nWhich day? Please type \'monday\', \'tuesday\', \'wednesday\', \'thursday\', \'friday\', \'saturday\' or \'sunday\'. If you don\'t want to filter at all please type \'all\'.\n').lower())
+                if day == 'monday' or day == 'tuesday' or day == 'wednesday' or day == 'thursday' or day == 'friday' or day == 'saturday' or day == 'sunday' or day == 'all':
+                    break
+                else:
+                    print('\nYou\'re input is not valid. Please close the program if you want to stop.\n')
+            except:
+                print('\nThat\'s not a valid value!\n')
+
+        print('\n{} is your choosen filter.\n'.format(day.title()))
+
+    # To get user input for month (all, january, february, ... , june)
+    if filter_criteria == 'month':
+        while True:
+            try:
+                month = str(input('Which month do you like to filter. Please type: January, February, March, April, Mai, June or all?\n').lower())
+                if month == 'january' or month == 'february' or month == 'march' or month == 'april' or month == 'may' or month == 'june' or month == 'all':
+                    break
+                else:
+                    print('\nYou\'re input is not valid. Please close the program if you want to stop.\n')
+            except:
+                print('\nThat\'s not a valid number!\n')
+
+        print('\n{} is your choosen filter.\n'.format(month.title()))
+
+    # To get user input for day of week (all, monday, tuesday, ... sunday)
+    if filter_criteria == 'day':
+        while True:
+            try:
+                day = str(input('\nWhich day? Please type \'monday\', \'tuesday\', \'wednesday\', \'thursday\', \'friday\', \'saturday\' or \'sunday\'. If you don\'t want to filter at all please type \'all\'.\n').lower())
+                if day == 'monday' or day == 'tuesday' or day == 'wednesday' or day == 'thursday' or day == 'friday' or day == 'saturday' or day == 'sunday' or day == 'all':
+                    break
+                else:
+                    print('\nYou\'re input is not valid. Please close the program if you want to stop.\n')
+            except:
+                print('\nThat\'s not a valid value!\n')
+
+        print('\n{} is your choosen filter.\n'.format(day.title()))
+
+    print('You\'re chosen filters are: \ncity = {} \nmonth = {} \nday = {}'.format(city, month, day))
 
     print('-'*40)
     return city, month, day
@@ -40,6 +122,27 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    # load data file into a dataframe
+
+    # convert the Start Time column to datetime
+
+    # extract month from Start Time to create new month column
+
+    # extract day of week from Start Time column to create new day column
+
+    # extract hour from the Start Time column to create an hour column
+
+
+    # filter by month if applicable
+
+    # use the index of the months list to get the corresponding int
+
+    # filter by month to create the new dataframe
+
+
+    # filter by day of week if applicable
+
+    # filter by day of week to create the new dataframe
 
 
     return df
